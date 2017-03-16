964393d5b3ebb568ff69d1d151a41c2df5ce64e1
Martin Abelson Sahlen <sahlen@Martins-MacBook-Pro.local>
2017-03-16 13:54:29 -0400

    Setup docopts

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/docopt/docopt-go"
+)
+
+func main() {
+	usage := `BigQuery Utilities.
+
+Usage:
+  bq-utils csv query <csv-query> <output-file>
+  bq-utils csv table <csv-table> <output-file>
+  bq-utils excel query <output-file> (<query> <sheetname>)...
+  bq-utils excel table <output-file> (<table> <sheetname>)...
+  bq-utils excel mixed <output-file> (<table>|<query> <sheetname>)...
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.`
+
+	arguments, _ := docopt.Parse(usage, nil, true, "BigQuery Utilities 0.0 Pre-Alpha", false)
+
+	fmt.Println(arguments)
 }
